069d1d815a0e07c0efbc99a47172b6a45d2fba0f
Henry Zhu <henryzhu@Henrys-MacBook-Pro-711.local>
2019-02-25 12:16:24 -0500

    setting up R markdown

--- ps_4.R
+++ ps_4.R
@@ -2,4 +2,17 @@ library(gt)
 library(devtools)
 library(tidyverse)
 library(readr)
-nc_poll<- read_csv("ps_4_elections-poll-nc09-3.csv")
+library(janitor)
+nc_poll<- read_csv("ps_4_elections-poll-nc09-3.csv")%>% clean_names()
+col_types = cols(
+  .default = col_character(),
+  turnout_scale = col_double(),
+  turnout_score = col_double(),
+  w_LV = col_double(),
+  w_RV = col_double(),
+  final_weight = col_double(),
+  timestamp = col_datetime(format = ""))
+View(nc_poll)
+dem<- nc_poll%>%
+  filter(response=="Dem")
+count(dem)
