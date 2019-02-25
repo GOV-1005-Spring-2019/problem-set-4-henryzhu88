8c75602c1c00b05259853dbcbcd6117b803b8f9a
Henry Zhu <henryzhu@Henrys-MacBook-Pro-711.local>
2019-02-25 14:33:13 -0500

    initial chart for P2 made

--- ps_4.R
+++ ps_4.R
@@ -3,6 +3,8 @@ library(devtools)
 library(tidyverse)
 library(readr)
 library(janitor)
+library(dplyr)
+library(lubridate)
 nc_poll<- read_csv("ps_4_elections-poll-nc09-3.csv")%>% clean_names()%>%
   col_types = cols(
     .default = col_character(),
@@ -11,13 +13,41 @@ nc_poll<- read_csv("ps_4_elections-poll-nc09-3.csv")%>% clean_names()%>%
     w_LV = col_double(),
     w_RV = col_double(),
     final_weight = col_double(),
-    timestamp = col_datetime(format = ""))
+    timestamp = col_datetime(format = "")),
+cols_label(
+  race_eth = "Race",
+  Dem = "DEM.",
+  Rep = "REP.",
+  Und = "UND."
+) %>%
+  fmt_percent(columns = vars(Dem, Rep, Und),
+              decimals = 0)
 dem<- nc_poll%>%
   filter(response=="Dem")
-count(dem)
 rep<- nc_poll%>%
   filter(response=="Rep")
-count(rep)
 und<- nc_poll%>%
   filter(response=="Und")
-count(und)
+gendiff<-nc_poll%>% filter(gender!= gender_combined)%>% select(gender,gender_combined)
+whitediff <-nc_poll%>%select(race_eth, file_race_black)%>%filter(race_eth=="White",race_eth!=file_race_black)
+reptime<-nc_poll%>%select(response,timestamp)%>%filter(response=="Rep")%>%head()
+reptime
+reptime<-nc_poll%>%select(response,timestamp)%>%filter(response=="Rep")%>%head(n=1)
+reptime
+demtime<-nc_poll%>%select(response,timestamp)%>%filter(response=="Dem")%>%head(n=1)
+demtime
+nc_poll%>%mutate(diffpartytime= interval(reptime,demtime))
+
+
+nc_poll%>%
+  select(response,race_eth, final_weight)%>%
+  group_by(race_eth,response)%>%
+  filter(response!="3", !race_eth=="[DO NOT READ] Don't know/Refused")%>%
+  summarize(total = sum(final_weight))%>%
+  spread(key =  response, value = total)%>%
+  mutate(all = Dem + Rep + Und) %>% 
+  mutate(Dem = Dem / all) %>% 
+  mutate(Rep = Rep / all) %>% 
+  mutate(Und = Und / all)%>%
+  select(-all)%>%
+  ungroup()
