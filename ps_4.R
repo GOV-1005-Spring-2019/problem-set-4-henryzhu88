e505d205754517082edb473b788ed3e16ce4c493
Henry Zhu <henryzhu@Henrys-MacBook-Pro-711.local>
2019-02-25 12:43:57 -0500

    answered Q1-Q3 of Problem 1

--- ps_4.R
+++ ps_4.R
@@ -3,16 +3,21 @@ library(devtools)
 library(tidyverse)
 library(readr)
 library(janitor)
-nc_poll<- read_csv("ps_4_elections-poll-nc09-3.csv")%>% clean_names()
-col_types = cols(
-  .default = col_character(),
-  turnout_scale = col_double(),
-  turnout_score = col_double(),
-  w_LV = col_double(),
-  w_RV = col_double(),
-  final_weight = col_double(),
-  timestamp = col_datetime(format = ""))
-View(nc_poll)
+nc_poll<- read_csv("ps_4_elections-poll-nc09-3.csv")%>% clean_names()%>%
+  col_types = cols(
+    .default = col_character(),
+    turnout_scale = col_double(),
+    turnout_score = col_double(),
+    w_LV = col_double(),
+    w_RV = col_double(),
+    final_weight = col_double(),
+    timestamp = col_datetime(format = ""))
 dem<- nc_poll%>%
   filter(response=="Dem")
 count(dem)
+rep<- nc_poll%>%
+  filter(response=="Rep")
+count(rep)
+und<- nc_poll%>%
+  filter(response=="Und")
+count(und)
